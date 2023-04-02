52001fd9e9deb0bcab397bf30f0fd60e04686800
satyakeerthi123 <128017098+satyakeerthi123@users.noreply.github.com>
2023-04-02 13:12:29 +0530

    Update MobileDetails.py.py

--- MobileDetails.py.py
+++ MobileDetails.py.py
@@ -70,12 +70,12 @@ if uploaded_file is not None:
     if st.checkbox("show number of mobiles with STORAGE above 64 and SCREENSIZE above 6"):
         st.write(data[(data.storage > 64) & (data.screensize > 6)])
     if st.checkbox("show number of mobiles below the price of 800($) dollars"):
-        st.write(data[data.price($) < 800])
+        st.write(data[data.price < 800])
     if st.checkbox("show number of mobiles with brand SAMSUNG and PRICE above $500 dollars"):
-        st.write(data[(data.Brand=='samsung') & (data.price($) >500)])
+        st.write(data[(data.Brand=='samsung') & (data.price >500)])
     if st.checkbox("show number of mobiles with brand VIVO and screensize above 6.5"):
         st.write(data[(data.Brand=='vivo')&(data.screensize > 6.5)])
     if st.checkbox("show number of mobiles with battery capacity above 4500 and price below 700 dollars"):
-        st.write(data[(data.battery>4500) & (data.price($) < 700)])
+        st.write(data[(data.battery>4500) & (data.price < 700)])
         
 
